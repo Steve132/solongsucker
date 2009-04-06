a2b330e669bb3be4849ff79bb8e36d6066db5621
sarahloewy <sarahloewy@0cb4d474-18c1-11de-989c-57ac4e9a12f8>
2009-04-06 17:16:08 +0000

    Added IOMacros.h, which contains a list of macros to make Workman's IO faster to implement.  Included are macros to get the open and close token of a class, Extract, Insert, and operators << and >> named OUT_OPERATOR and IN_OPERATOR respectively.
    Added Hand, mostly as an example of proper use of IOMacros.h, and to get 
    started on adding base classes.
    
    Added Chip, for convenience.  It is just a typedef of an unsigned, which 
    can be converted to a class later if needed.
    
    Took approximately 45 minutes to make the classes and macros, 15 minutes 
    to verify.

---
+++ trunk/src/Hand.h
@@ -0,0 +1,28 @@
+#ifndef _HAND
+#define _HAND
+
+#include "Chip.h"
+#include <iostream>
+#include <set>
+
+class Hand : public std::multiset<Chip>
+{
+	public:
+		Hand();
+		~Hand();
+
+		// To fulfill OODataDesign.doc
+		friend std::ostream&  operator<<(std::ostream& fout, Hand& hand);
+		friend std::ifstream& operator>>(std::ifstream& fin, Hand& hand);
+
+		virtual void Extract(std::ifstream& fin);
+		virtual void Insert(std::ostream& fout);
+
+	protected:
+		// To fulfill OODataDesign.doc
+		virtual void Put(std::ostream& fout);
+		virtual void Get(std::ifstream& fin);
+};
+
+#endif
+
