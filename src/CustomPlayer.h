6ca1f280de9713071966feaf32290810873dfdda
sarahloewy <sarahloewy@0cb4d474-18c1-11de-989c-57ac4e9a12f8>
2009-04-07 02:49:05 +0000

    Added CustomPlayer base code with an outline for a property enum which can be used for determining aggressivenes, acceptiveness, ignorance, etc. to be flushed out later.
    Took approximately 15 minutes.

---
+++ src/CustomPlayer.h
@@ -0,0 +1,43 @@
+#ifndef _CUSTOMPLAYER
+#define _CUSTOMPLAYER
+
+#include "Bargain.h"
+#include "Chip.h"
+#include "Player.h"
+
+class CustomPlayer : public Player
+{
+	public:
+		enum property
+		{
+			AGGRESSIVENESS,
+			ACCEPTIVENESS,
+			IGNORANCE,		// etc.
+			NUM_PROPERTIES
+		};
+
+		CustomPlayer();
+		CustomPlayer(unsigned sizeOfHand, Chip id, property* properties = NULL);
+		~CustomPlayer();
+
+		virtual Bargain* AcceptBargainAccept();
+		virtual Bargain* AcceptBargainReject();
+
+		// To fulfill OODataDesign.doc
+		friend std::ostream&  operator<<(std::ostream& fout, Stack& stack);
+		friend std::ifstream& operator>>(std::ifstream& fin, Stack& stack);
+	
+		virtual void Extract(std::ifstream& fin);
+		virtual void Insert(std::ostream& fout);
+
+	protected:
+		// To fulfill OODataDesign.doc
+		virtual void Get(std::ifstream& fin);
+		virtual void Put(std::ostream& fout);
+
+	private:
+		property properties[NUM_PROPERTIES];
+};
+
+#endif
+
