ecb0de81ba243b20feb66551e8abe762f37f9bd4
sarahloewy <sarahloewy@0cb4d474-18c1-11de-989c-57ac4e9a12f8>
2009-04-07 02:34:39 +0000

    Added Player and Board as base classes, based on the meeting on April 5.
    Took approximately 15 minutes.

---
+++ trunk/src/Board.cpp
@@ -0,0 +1,38 @@
+#include "Board.h"
+
+Board::Board()
+{
+}
+
+Board::~Board()
+{
+}
+
+void Board::Initialize()
+{
+}
+
+void Board::Simulate()
+{
+}
+
+void Board::WrapUp()
+{
+}
+
+SimMgmt::Message* Board::AcceptChipsDiscard(std::multiset<Chip> chips)
+{
+}
+
+INSERT(Board)
+
+PUT(Board)
+
+EXTRACT(Board)
+
+GET(Board)
+
+OUT_OPERATOR(Board)
+
+IN_OPERATOR(Board)
+
---
+++ trunk/src/Board.h
@@ -0,0 +1,41 @@
+#ifndef _BOARD
+#define _BOARD
+
+#include "Chip.h"
+#include "Player.h"
+#include "SimMgmt.h"
+#include "Stack.h"
+#include <set>
+
+class Board
+{
+	public:
+		Board();
+		~Board();
+	
+		// Styled after Workman's Conversation class
+		void Initialize();
+		void Simulate();	// Performs rule checking
+		void WrapUp();
+
+		SimMgmt::Message* AcceptChipsDiscard(std::multiset<Chip> chips);
+
+		// To fulfill OODataDesign.doc
+		friend ostream&  operator<<(ostream& fout, Bargain& bargain);
+		friend ifstream& operator>>(ifstream& fin, Bargain& bargain);
+	
+		virtual void Extract(std::ifstream& fin);
+		virtual void Insert(std::ostream& fout);
+
+	protected:
+		// To fulfill OODataDesign.doc
+		virtual void Put(std::ostream& fout);
+		virtual void Get(std::ifstream& fin);
+
+	private:
+		std::set<Player> players;
+		std::list<Stack> stacks;
+};
+
+#endif
+
