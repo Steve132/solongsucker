640ebfd1b65b8bcd721fcc8b654d78ad036e7041
sarahloewy <sarahloewy@0cb4d474-18c1-11de-989c-57ac4e9a12f8>
2009-04-13 06:50:49 +0000

    Added ChipMsg.  Moved Stack to Pile.  Took approximately 1 minute, since I counted this in the previous revision, but improperly used subversion.

---
+++ trunk/src/ChipMsg.h
@@ -0,0 +1,31 @@
+#ifndef _CHIPMSG
+#define _CHIPMSG
+
+#include "Chip.h"
+#include "SimMgmt.h"
+
+class ChipMsg : public SimMgmt::Message
+{
+	public:
+		ChipMsg(const int Handler, const std::string Description);
+		ChipMsg(const int Handler, const std::string Description, const Chip chipIn);
+		~ChipMsg();
+	
+		// To fulfill OODataDesign.doc
+		friend ostream&  operator<<(ostream& fout, ChipMsg& chipMsg);
+		friend ifstream& operator>>(ifstream& fin, ChipMsg& chipMsg);
+	
+		virtual void Extract(std::ifstream& fin);
+		virtual void Insert(std::ostream& fout);
+
+	protected:
+		// To fulfill OODataDesign.doc
+		virtual void Put(std::ostream& fout);
+		virtual void Get(std::ifstream& fin);
+	
+	private:
+		Chip chip;
+};
+
+#endif
+
---
+++ trunk/src/Pile.cpp
@@ -0,0 +1,23 @@
+#include "Pile.h"
+#include "IOMacros.h"
+
+Pile::Pile()
+{
+}
+
+Pile::~Pile()
+{
+}
+
+INSERT(Pile)
+
+PUT(Pile)
+
+EXTRACT(Pile)
+
+GET(Pile)
+
+OUT_OPERATOR(Pile)
+
+IN_OPERATOR(Pile)
+
