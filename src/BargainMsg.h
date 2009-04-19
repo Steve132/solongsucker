169b41e44d81260a7f7a0d352aa8c9add6a648c9
sarahloewy <sarahloewy@0cb4d474-18c1-11de-989c-57ac4e9a12f8>
2009-04-19 16:08:25 +0000

    Added BargainMsg.  Took 10 minutes.

---
+++ src/BargainMsg.h
@@ -0,0 +1,31 @@
+#ifndef _BARGAINMSG
+#define _BARGAINMSG
+
+#include "Bargain.h"
+#include "SimMgmt.h"
+
+class BargainMsg : SimMgmt::Message
+{
+	public:
+		BargainMsg(int Handler, std::string Description);
+		BargainMsg(int Handler, std::string Description, const Bargain& bargainIn);
+		~BargainMsg();
+
+		// To fulfill OODataDesign.doc
+		friend ostream&  operator<<(ostream& fout, Bargain& bargain);
+		friend ifstream& operator>>(ifstream& fin, Bargain& bargain);
+	
+		virtual void Extract() throw(IOMgmt::TokenError);
+		virtual void Insert();
+
+	protected:
+		// To fulfill OODataDesign.doc
+		virtual void Put();
+		virtual void Get() throw(IOMgmt::TokenError);
+
+	private:
+		Bargain bargain;
+};
+
+#endif
+
