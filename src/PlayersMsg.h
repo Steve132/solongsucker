579f8149c8761a4aff5d981cca73f0d7a5fd35b2
steve@soapforge.com <steve@soapforge.com@0cb4d474-18c1-11de-989c-57ac4e9a12f8>
2009-04-23 22:14:26 +0000

    Implemented PlayersMsg partway

---
+++ src/PlayersMsg.h
@@ -0,0 +1,27 @@
+#ifndef PLAYERSMSG_H
+#define PLAYERSMSG_H
+#include "IOMgmt.h"
+#include "SimMgmt.h"
+
+class PlayersMsg : public SimMgmt::Message
+{  // Inherits from Message
+public:
+	PlayersMsg(int MaxAgents) throw(AppError);
+	~PlayersMsg();
+	int       getNumAgents();
+	SimMgmt::Agent* getAgent(string playerid) throw(AppError);
+	SimMgmt::Agent** getOthers(int Num) throw(AppError);
+	void      setAgent(string playerid, SimMgmt::Agent* agent) throw(AppError);
+
+protected:
+	//Inherited as virtual and must be redefined:
+	virtual void Insert();//Must write to the default program output stream
+	virtual void Put();   //Must write to the default program output stream
+private:
+	int      numAgents;
+	int      lastAgent;
+	SimMgmt::Agent* *agents;
+	string   *agentNames;
+}; //Players
+
+#endif
